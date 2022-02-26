058a3a8cf53b49e72aa94ed42088508f3e65dd52
AntonioAlgarin <antonio@cybermindedvets.com>
2022-02-25 16:57:41 -0800

    added styling to footer component

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  CardText,
+  Button,
+  CardFooter,
+} from "reactstrap";
 
 export default class Footer extends Component {
   render() {
-    return <h4>Footer</h4>;
+    return (
+      <div>
+        <Card>
+          <CardHeader tag="h3">
+            What do I want to add here?
+          </CardHeader>
+            <CardBody>
+              <CardTitle tag="h5">
+                Pantry Hero
+              </CardTitle>
+          <Button>
+            Learn More
+          </Button>
+            </CardBody>
+          <CardFooter className="text-muted">
+            Team Uncapped
+          </CardFooter>
+        </Card>
+      </div>
+    )
   }
 }
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import "../../../assets/stylesheets/Home.css"
 
 export default class Home extends Component {
   render() {
     return (
-      <>
-        <h2>This is Home Page</h2>
-      </>
-    );
+      <div className="HomeBackgroundImage">
+      <h2>This is Home</h2>
+      <p>Longer description of why you should check out our app</p>
+      </div>
+    )
   }
 }
