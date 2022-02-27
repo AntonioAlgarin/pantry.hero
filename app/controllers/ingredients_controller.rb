8bdab224134f2e45aa57f2561f39826d6bec5240
Chava888 <salvador_tena@hotmail.com>
2022-02-26 23:41:08 -0800

    uncomment controller -index & patch

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,12 +1,12 @@
 class IngredientsController < ApplicationController
     def index
-    #  if user_signed_in?
+     if user_signed_in?
         user = User.find(params[:user_id])
         pantry = user.ingredients.all
         render json: pantry
-    #    else
-    #     render json: 'please log-in you are not authorized'
-    #    end
+       else
+        render json: 'please log-in you are not authorized'
+       end
     end
     def create
         if user_signed_in?
@@ -18,14 +18,14 @@ class IngredientsController < ApplicationController
         end
     end
     def update
-        # if user_signed_in?
+        if user_signed_in?
             user = User.find(params[:user_id])
             ingredient = user.ingredients.find(params[:id])
             ingredient.update(ingredient_params)
             render json: ingredient
-        # else
-        #     render json: 'please log-in you are not authorized'
-        # end
+        else
+            render json: 'please log-in you are not authorized'
+        end
     end
     def destroy
         if user_signed_in?
